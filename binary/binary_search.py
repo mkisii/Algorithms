1d2c22a54e6bdecca214683d0f0e9a0466153cbf
obedondimu <ondimuobed@gmail.com>
2022-05-17 20:11:34 +0300

    Adding Comments

--- binary/binary_search.py
+++ binary/binary_search.py
@@ -1,21 +1,30 @@
-def binary_search(my_list,item):
+
+my_list = [1,2,3,4,5]
+def binary_search(list,item):
+    # The index of the first value in the list[]
     low = 0
-    high = len(my_list) - 1
+    # The last index in the list
+    high = len(list) - 1
 
     while low <= high:
+        # Mid index of the list
+        mid =(low + high) //2
 
-        mid = (low + high)
-        guess = my_list[mid]
+       #The value of the middle index
+        guess = list[mid]
 
+        # Compare if the value gussed is equal to the item being searched
         if guess == item:
             return mid
+
+        # Compare if value being searched is greate than the gussed value
         if guess > item:
-            high = mid- 1
+            high = mid - 1
+
         else:
             low = mid + 1
 
+
     return None
 
-list = [1,2,3,4,5,6,7,8,9,10]
-
-print( "Index Number:", binary_search(list,10))
+print( "Index value:", binary_search(my_list,2))
